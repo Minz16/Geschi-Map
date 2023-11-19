f7b425ccef789addd49234c0f8828b99c04761b7
Minz16 <minzinli953@gmail.com>
2023-11-19 18:54:21 +0100

    Veränderter Hintergrund und erstmaliges Grundgerüst

--- script.js
+++ script.js
@@ -58,28 +58,23 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 
-//var parlamentsPlatz =  L.marker([50.12, 8.71]).addTo(map)
-//parlamentsPlatz.bindPopup(
-//"Hallo, ich bin ein Popup mit <a href='https://google.com'>Link</a>"
-//).openPopup()
+
+// name = new Circle("name", [50.24, -3.5], "text", radius, "farbe in Hexcode").create()
+
+// name = new Pointer("name", [50.12, 8.71], "text").create()
 
 
 
 
-
-
+kreis = new Circle("kreis", [50.12, 8.71], "Ich bin ein kreis", 600, "blue").create()
 
 
 parlament = new Pointer("parlament", [50.12, 8.71], "Hallo, ich bin ein Popup mit <a href='https://google.com'>Link</a>").create()
+Fortnite = new Pointer("Fortnite",[50.122084372545245, 8.702001261031475], "Best gamer (nach Min Zin) Für Fortnite" ).create()
 
 
 
 
-
-
-
-
-//.openPopup öffnet das PopUp sofort
 
 
 
